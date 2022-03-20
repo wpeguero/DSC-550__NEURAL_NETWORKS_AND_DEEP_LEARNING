476a1c1a2dd859c552bdcaee407be46f05da6a3d
wpeguero <wpeguerorosario@outlook.com>
2022-03-20 15:14:04 -0400

    add(base): added resources for class projects.
    The initial project resources have been added to supplement the class.

---
+++ DSC-550-RS-ThompsonSampling-Project1.py
@@ -0,0 +1,80 @@
+#MIT License
+
+#Copyright (c) 2019 Packt
+
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+
+#The above copyright notice and this permission notice shall be included in all
+#copies or substantial portions of the Software.
+
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#SOFTWARE.
+
+# AI for Sales & Advertizing - Sell like the Wolf of AI Street
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+
+# Setting the parameters
+N = 10000
+d = 9
+
+# Building the environment inside a simulation
+conversion_rates = [0.05,0.13,0.09,0.16,0.11,0.04,0.20,0.08,0.01]
+X = np.array(np.zeros([N,d]))
+for i in range(N):
+    for j in range(d):
+        if np.random.rand() <= conversion_rates[j]:
+            X[i,j] = 1
+
+# Implementing Random Selection and Thompson Sampling
+strategies_selected_rs = []
+strategies_selected_ts = []
+total_reward_rs = 0
+total_reward_ts = 0
+numbers_of_rewards_1 = [0] * d
+numbers_of_rewards_0 = [0] * d
+for n in range(0, N):
+    # Random Selection
+    strategy_rs = random.randrange(d)
+    strategies_selected_rs.append(strategy_rs)
+    reward_rs = X[n, strategy_rs]
+    total_reward_rs = total_reward_rs + reward_rs
+    # Thompson Sampling
+    strategy_ts = 0
+    max_random = 0
+    for i in range(0, d):
+        random_beta = random.betavariate(numbers_of_rewards_1[i] + 1, numbers_of_rewards_0[i] + 1)
+        if random_beta > max_random:
+            max_random = random_beta
+            strategy_ts = i
+    reward_ts = X[n, strategy_ts]
+    if reward_ts == 1:
+        numbers_of_rewards_1[strategy_ts] = numbers_of_rewards_1[strategy_ts] + 1
+    else:
+        numbers_of_rewards_0[strategy_ts] = numbers_of_rewards_0[strategy_ts] + 1
+    strategies_selected_ts.append(strategy_ts)
+    total_reward_ts = total_reward_ts + reward_ts
+
+# Computing the Relative Return
+relative_return = (total_reward_ts - total_reward_rs) / total_reward_rs * 100
+print("Relative Return: {:.0f} %".format(relative_return))
+
+# Plotting the Histogram of Selections
+plt.hist(strategies_selected_ts)
+plt.title('Histogram of Selections')
+plt.xlabel('Strategy')
+plt.ylabel('Number of times the strategy was selected')
+plt.show()
